2bf6bcbf3abe89e4e361e9dc1802c95ed3612e07
IDDimov21 <IDDimov21@codingburgas.bg>
2025-01-22 17:27:41 +0200

    create 3 layer project structure + login/register design & auth

---
+++ BlazorApp1/FestivalApp_PL/Auth/CutstomAuthStateProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private ClaimsPrincipal _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        return Task.FromResult(new AuthenticationState(_currentUser));
+    }
+
+    public void Login(string email)
+    {
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, email)
+        }, "auth");
+
+        _currentUser = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_currentUser)));
+    }
+
+    public void Logout()
+    {
+        _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_currentUser)));
+    }
+}
--- BlazorApp1/FestivalApp_PL/Program.cs
+++ BlazorApp1/FestivalApp_PL/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorApp1;
+using FestivalApp_PL;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app"); // Ensure this line exists!
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Authentication Services
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
