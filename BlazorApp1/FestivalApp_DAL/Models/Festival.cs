6b57b8c0e705090543eb46cd08c574438825df90
IDDimov21 <IDDimov21@codingburgas.bg>
2025-01-26 13:27:25 +0200

    fix bugs with sidebar + ui

---
+++ BlazorApp1/FestivalApp_DAL/Models/Festival.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FestivalApp_DAL.Models
+{
+    public class Festival
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public int ArtistId { get; set; } // Foreign Key to Artist
+        
+        [Required]
+        public string FestivalName { get; set; }
+        
+        [Required]
+        public DateTime Date { get; set; }
+        
+        [Required]
+        public string Time { get; set; }
+        
+        [Required]
+        public double TicketCost { get; set; }
+    }
+}
--- BlazorApp1/FestivalApp_PL/Program.cs
+++ BlazorApp1/FestivalApp_PL/Program.cs
@@ -1,19 +1,32 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http;
 using FestivalApp_PL;
 using FestivalApp_PL.Auth;
-using System.Net.Http.Json;
+using FestivalApp_BLL.Services;
+using FestivalApp_DAL.Repository;
+using FestivalApp_DAL.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// ✅ Register HttpClient
+// Add HttpClient
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5148/") });
 
-// ✅ Add Authentication Services
+// Add Authentication Services
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+// Database Connection (SQLite)
+var connectionString = "Data Source=festivalapp.db";
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+// Register Services
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 await builder.Build().RunAsync();
